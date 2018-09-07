b47fd1dbf7ca12b77b8dd9bd1749c748c4d952d7
dog <dog@DESKTOP-J0UL22P>
2018-09-07 15:47:13 +0900

    180906 Study_Team

--- 11_API/src/kh/java/api/token/TokenTest.java
+++ 11_API/src/kh/java/api/token/TokenTest.java
@@ -1,5 +1,6 @@
 package kh.java.api.token;
 
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class TokenTest {
@@ -7,6 +8,39 @@ public class TokenTest {
 	static char[] carr;
 
 	public static void main(String[] args) {
+//		String str = "J a v a P r o g r a m ";
+//		
+//		char carr[] = str.toCharArray();
+//		System.out.println("토큰 처리전 글자 출력 ========" );
+//		for(int i=0; i<carr.length; i++)
+//		{
+//			System.out.print(carr[i]);
+//		}
+//		System.out.println(" \n\n전 글자갯수 출력 ========");
+//		System.out.println(str.length());
+//		System.out.println("\n토큰 처리 후 글자갯수 출력 확인 ========");
+//		StringTokenizer stn = new StringTokenizer(str, " ");
+//		while(stn.hasMoreTokens())
+//		{
+//			carr = (stn.nextToken()).toCharArray();
+//		}
+//		
+//		System.out.println(carr.length);
+//		for(int i=0; i<carr.length; i++)
+//		{
+//			System.out.println(carr[i]);			
+//		}
+//		System.out.println(temp.length());
+////		carr = temp.toCharArray();
+//		System.out.println(carr.length);
+//		String str2;
+//		str2 = String.valueOf(carr);
+//		str2.toUpperCase();
+//		System.out.println(str2);
+
+		
+		
+		
 		String str = "J a v a P r o g r a m ";
 		
 		char carr[] = str.toCharArray();
@@ -19,13 +53,22 @@ public class TokenTest {
 		System.out.println(str.length());
 		System.out.println("\n토큰 처리 후 글자갯수 출력 확인 ========");
 		StringTokenizer stn = new StringTokenizer(str, " ");
+		int cnt = 0;
 		while(stn.hasMoreTokens())
 		{
-			temp += stn.nextToken();
+			carr[cnt++] = stn.nextToken().charAt(0);
 		}
 		System.out.println(temp.length());
 		carr = temp.toCharArray();
 		System.out.println(carr.length);
+		String str2;
+		str2 = String.valueOf(carr);
+		str2.toUpperCase();
+		System.out.println(str2);
+		
+		
 	}
+	
+	
 
 }
